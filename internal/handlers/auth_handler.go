008e5ddd747112b6b0f08f73150610f089752399
bertoxic <dirusalbert@gmail.com>
2024-10-22 16:18:29 +0100

    removed henry
    Took 2 minutes

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -1,10 +1,7 @@
 package handlers
 
 import (
-	"context"
-	"fmt"
 	"github.com/bertoxic/graphqlChat/internal/database"
-	errorx "github.com/bertoxic/graphqlChat/internal/error"
 	"github.com/bertoxic/graphqlChat/internal/models"
 	"github.com/bertoxic/graphqlChat/internal/render"
 	"github.com/bertoxic/graphqlChat/pkg/config"
@@ -26,14 +23,14 @@ func NewRepo(repository *Repository) {
 	Repo = *repository
 }
 func (au *Repository) HomePage(w http.ResponseWriter, r *http.Request) {
-	ctx := context.Context(context.Background())
-	user, err := au.db.GetUserByEmail(ctx, "henry@mail.com")
-	if err != nil {
-		fmt.Printf("", errorx.New(errorx.ErrCodeDatabase, "", err))
-		return
-	}
+	//ctx := context.Context(context.Background())
+	//user, err := au.db.GetUserByEmail(ctx, "henry@mail.com")
+	//if err != nil {
+	//	fmt.Printf("", errorx.New(errorx.ErrCodeDatabase, "", err))
+	//	return
+	//}
 	render.Template(w, "home.page.gohtml", &models.TemplateData{
-		StringMap: map[string]string{"name": user.UserName},
+		StringMap: map[string]string{"name": "user.UserName"},
 	})
 
 }
